6f196df065650dc0dc2c044dcf440831de951bd9
Tyler Olmsted <profbass@gmail.com>
2024-03-14 02:02:54 -0600

    Router uodates

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,16 @@ var router = express.Router();
 const app = express();
 const port = 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+// Use the router in your application
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+module.exports = app;
